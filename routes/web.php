491ae25e28b6a4d9475fe0f48a3079e7e67852f7
itstaff1027 <itstaffdelrosario@gmail.com>
2023-10-05 12:17:44 +0800

    test again

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
