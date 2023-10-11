9f3387532e170e96dfbd3131a186328db086978b
itstaff1027 <itstaffdelrosario@gmail.com>
2023-10-11 16:55:47 +0800

    migration problem

--- database/migrations/2023_10_05_071159_create_materials_table.php
+++ database/migrations/2023_10_05_071159_create_materials_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name', 255)->default('')->nullable();
             $table->string('color', 255)->default('')->nullable();
             $table->string('size', 255)->default('')->nullable();
-            $table->string('unit_per', 255)->default('')->nullable();
             $table->timestamps();
         });
     }
