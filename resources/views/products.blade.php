a2a30c5e99f325fe5958eaad8f2271d508d4cac9
itstaff1027 <itstaffdelrosario@gmail.com>
2023-11-23 11:51:31 +0800

    adding a wire navigate to make it more look like a spa app

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,16 +4,16 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Products') }}
             </h2>
-            <a href="{{ route('list-barcodes') }}" class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <a href="{{ route('list-barcodes') }}" wire:navigate class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 {{ __('Barcodes') }}
             </a>
-            <a href="{{ route('product-logs') }}" class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <a href="{{ route('product-logs') }}" wire:navigate class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 {{ __('Product Logs') }}
             </a>
-            <a href="{{ route('sales-logs') }}" class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <a href="{{ route('sales-logs') }}" wire:navigate class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 {{ __('Sales Logs') }}
             </a>
-            <a href="{{ route('summary-outgoing') }}" class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <a href="{{ route('summary-outgoing') }}" wire:navigate class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 {{ __('Summary') }}
             </a>
         </div>
