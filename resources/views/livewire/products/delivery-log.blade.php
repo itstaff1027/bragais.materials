036870cadc319ce11ea9c49c7fe149e8633e10f5
itstaff1027 <itstaffdelrosario@gmail.com>
2023-12-07 16:55:44 +0800

    resolve conflict keeping alive delivery log page

--- resources/views/livewire/products/delivery-log.blade.php
+++ resources/views/livewire/products/delivery-log.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="p-0 w-full h-96 overflow-auto" wire:poll.30s>
+    <div class="p-0 w-full h-96 overflow-auto" wire:poll.keep-alive.5s>
         <div class="flex flex-col justify-center items-center w-full">
             <div class="flex justify-center items-center bg-black text-white w-full p-2">
                 <h1 class="text-xl font-serif">PENDING</h1>
